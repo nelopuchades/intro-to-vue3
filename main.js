00ba46beea06d7ac837ad07e107b9a269f974fa0
Nelo Puchades <correo@nelo.dev>
2021-10-27 21:03:52 +0200

    Creating vue app + Rendering things + Binding attributes

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://www.amazon.es",
+    };
+  },
+});
