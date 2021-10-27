2c2ebe3781c9193a803ee15891aa282fa09dfb58
Nelo Puchades <correo@nelo.dev>
2021-10-28 00:11:57 +0200

    Playing with forms and v-models

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,8 @@ app.component("product-display", {
             </button>
           </div>
         </div>
+        <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+        <review-form @review-submitted="addReview"></review-form>
       </div>`,
   data() {
     return {
@@ -60,6 +62,7 @@ app.component("product-display", {
           quantity: 0,
         },
       ],
+      reviews: [],
     };
   },
   methods: {
@@ -68,6 +71,9 @@ app.component("product-display", {
     },
     updateVariant(index) {
       this.selectedVariant = index;
+    },
+    addReview(newReview) {
+      this.reviews.push(newReview);
     },
   },
   computed: {
