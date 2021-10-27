18f9ebd5e956770036741db7251e799665a47c91
Nelo Puchades <correo@nelo.dev>
2021-10-27 21:14:04 +0200

    Playing with conditional and list rendering

--- main.js
+++ main.js
@@ -2,8 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
-      image: "./assets/images/socks_green.jpg",
+      image: "./assets/images/socks_blue.jpg",
       url: "https://www.amazon.es",
+      inStock: true,
+      onSale: true,
+      inventory: 8,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      sizes: ["XXXL", "XXL", "XL", "L"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
     };
   },
 });
